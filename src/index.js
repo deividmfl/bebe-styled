0a741213c6da82a4160dbb1b78ff382ed619249d
Deivid Linhares :duck <deividmflinhares2@gmail.com>
2019-08-14 15:00:15 -0300

    export Collapse and Navbar component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import Container from './Container';
 import Row from './Row';
 import Col from './Col';
 import Nav from './Nav';
+import Navbar from './Navbar';
+import Collapse from './Collapse';
 
 export {
   Container,
   Row,
   Col,
   Nav,
+  Navbar,
+  Collapse,
 }
