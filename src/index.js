53ad7fb9d2f28eabef957323693060438dcf4dbd
Deivid Linhares :duck <deividmflinhares2@gmail.com>
2019-08-19 10:50:11 -0300

    export modal component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import Container from './Container';
-import Row from './Row';
 import Col from './Col';
+import Collapse from './Collapse';
+import Container from './Container';
+import Modal from './Modal';
 import Nav from './Nav';
 import Navbar from './Navbar';
 import NavbarBrand from './NavbarBrand';
 import NavbarToggler from './NavbarToggler';
 import NavItem from './NavItem';
 import NavLink from './NavLink';
-import Collapse from './Collapse';
+import Row from './Row';
 
 export {
-  Container,
-  Row,
   Col,
+  Collapse,
+  Container,
+  Modal,
   Nav,
   Navbar,
   NavbarBrand,
   NavbarToggler,
   NavItem,
   NavLink,
-  Collapse,
+  Row,
 }
